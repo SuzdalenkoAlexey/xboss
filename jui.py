49219fc9f6a8d5348a16c9886da199b79ab9a895
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2023-09-02 20:43:10 +0200

    jijij

---
+++ jui.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+
+
+sender_email = "xboss.developer@gmail.com"
+receiver_email = "xboss.developer@gmail.com"
+password = "suzdalenko"
+
+message = {}
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+# Create the plain-text and HTML version of your message
+text = """sdfgsdfgsdfg
+"""
+
+
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, "message.as_string()"
+    )
