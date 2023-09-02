ee814892dea0c5adfe41c34dccb208cd426d721f
Alexey Suzdalenko <suzdalenko.suzdalenko@gmail.com>
2023-09-02 21:29:24 +0200

    jijij

--- jui.py
+++ jui.py
@@ -1,25 +1,25 @@
-import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import socket
 
-
-sender_email = "xboss.developer@gmail.com"
+sender_email   = "xboss.developer@gmail.com"
 receiver_email = "xboss.developer@gmail.com"
-password = "doneqrmjfhbxlpwv"
+password       = "doneqrmjfhbxlpwv"
+hostname       = socket.gethostname()
+ip_address     = socket.gethostbyname(hostname)
 
-message = {}
-message["Subject"] = "multipart test"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-# Create the plain-text and HTML version of your message
-text = """sdfgsdfgsdfg
-"""
-
+message            = MIMEMultipart("alternative")
+message["Subject"] = ip_address+' '+hostname
+message["From"]    = sender_email
+message["To"]      = receiver_email
+text               = ip_address+' '+hostname
+part1              = MIMEText(text, "plain")
+message.attach(part1)
 
 
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, "message.as_string()"
-    )
+    server.sendmail( sender_email, receiver_email, message.as_string())
+    server.quit()
+
