17cee4e04f0725983b9d66bc2d92a43bd15c391d
Alexey Suzdalenko <suzdalenko.suzdalenko@gmail.com>
2023-09-04 08:37:16 +0200

    jjijiji jiji ij i

--- jui.py
+++ jui.py
@@ -1,7 +1,10 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import json
 import smtplib
 import socket
+import requests
+
 
 sender_email   = "xboss.developer@gmail.com"
 receiver_email = "xboss.developer@gmail.com"
@@ -10,13 +13,22 @@ hostname       = socket.gethostname()
 ip_address     = socket.gethostbyname(hostname)
 
 message            = MIMEMultipart("alternative")
-message["Subject"] = ip_address+' '+hostname
 message["From"]    = sender_email
 message["To"]      = receiver_email
 text               = ip_address+' '+hostname
-part1              = MIMEText(text, "plain")
-message.attach(part1)
 
+ipR = None
+try:
+    ipR = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=f9dd9c2cea00459bb251f3ea4bb22d19&ip=46.222.34.204')
+    ipR = ipR.json()
+    ipR = ipR['ip']+" "+ipR['continent_name']+" "+ipR['country_name']+" "+ipR['city']
+    text = ipR
+    message["Subject"] = ipR
+except:
+    message["Subject"] = ip_address+' '+hostname
+
+part1 = MIMEText(text, "plain")
+message.attach(part1)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(sender_email, password)
